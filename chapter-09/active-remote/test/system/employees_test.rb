655292c18caf9ad8cc2b526389258a25e772db99
Kevin Watson <kevin@kevin.watson.name>
2023-10-20 12:19:09 -0600

    New rails 6 apps for chap 9

---
+++ chapter-09/active-remote/test/system/employees_test.rb
@@ -0,0 +1,47 @@
+require "application_system_test_case"
+
+class EmployeesTest < ApplicationSystemTestCase
+  setup do
+    @employee = employees(:one)
+  end
+
+  test "visiting the index" do
+    visit employees_url
+    assert_selector "h1", text: "Employees"
+  end
+
+  test "creating a Employee" do
+    visit employees_url
+    click_on "New Employee"
+
+    fill_in "First name", with: @employee.first_name
+    fill_in "Guid", with: @employee.guid
+    fill_in "Last name", with: @employee.last_name
+    click_on "Create Employee"
+
+    assert_text "Employee was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Employee" do
+    visit employees_url
+    click_on "Edit", match: :first
+
+    fill_in "First name", with: @employee.first_name
+    fill_in "Guid", with: @employee.guid
+    fill_in "Last name", with: @employee.last_name
+    click_on "Update Employee"
+
+    assert_text "Employee was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Employee" do
+    visit employees_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Employee was successfully destroyed"
+  end
+end
