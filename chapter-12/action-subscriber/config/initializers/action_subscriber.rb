9bd0a7a727b2eaca9a3bc516eb2e7870361a1a1d
Kevin Watson <kevin@kevin.watson.name>
2023-10-27 11:02:04 -0600

    Upgrading chap 12 to Rails 6

---
+++ chapter-12/action-subscriber/config/initializers/action_subscriber.rb
@@ -0,0 +1,8 @@
+ActionSubscriber.draw_routes do
+  default_routes_for EmployeeSubscriber
+end
+
+ActionSubscriber.configure do |config|
+  config.hosts = ["host.docker.internal"]
+  config.port = 5672
+end
---
+++ chapter-12/active-publisher/config/initializers/active_publisher.rb
@@ -0,0 +1,3 @@
+require "active_publisher"
+
+::ActivePublisher::Configuration.configure_from_yaml_and_cli
